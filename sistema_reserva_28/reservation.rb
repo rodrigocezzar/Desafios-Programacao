6827fd28f173c0c5b4731c3241752e94109bbdbb
rodrigo_abreu <rodrigocezzar85@gmail.com>
2023-11-22 10:54:58 -0300

    fix/rename variables and methods

--- sistema_reserva_28/reservation.rb
+++ sistema_reserva_28/reservation.rb
@@ -1,44 +1,44 @@
 # frozen_string_literal: true
 
 class Hotel
-  attr_accessor :quartos_disponiveis, :quartos_ocupados
+  attr_accessor :available_rooms, :occupied_rooms
 
   def initialize
-    @quartos_disponiveis = [1, 2, 3, 4, 5]
-    @quartos_ocupados = []
+    @available_rooms = [1, 2, 3, 4, 5]
+    @occupied_rooms = []
   end
 
-  def reservar(quarto)
-    unless @quartos_disponiveis.include?(quarto)
-      puts "O quarto #{quarto} não está disponível."
+  def reserve(room)
+    unless @available_rooms.include?(room)
+      puts "O quarto #{room} não está disponível."
       return
     end
 
-    @quartos_disponiveis.delete(quarto)
+    @available_rooms.delete(room)
 
-    @quartos_ocupados << quarto
+    @occupied_rooms << room
 
-    puts "O quarto #{quarto} foi reservado."
+    puts "O quarto #{room} foi reservado."
   end
 
-  def cancelar(quarto)
-    unless @quartos_ocupados.include?(quarto)
-      puts "O quarto #{quarto} não está ocupado."
+  def cancel(room)
+    unless @occupied_rooms.include?(room)
+      puts "O quarto #{room} não está ocupado."
       return
     end
 
-    @quartos_ocupados.delete(quarto)
+    @occupied_rooms.delete(room)
 
-    @quartos_disponiveis << quarto
+    @available_rooms << room
 
-    puts "O quarto #{quarto} foi cancelado."
+    puts "O quarto #{room} foi cancelado."
   end
 
-  def buscar(quarto)
-    if @quartos_disponiveis.include?(quarto)
-      puts "O quarto #{quarto} está disponível."
+  def search(room)
+    if @available_rooms.include?(room)
+      puts "O quarto #{room} está disponível."
     else
-      puts "O quarto #{quarto} está ocupado."
+      puts "O quarto #{room} está ocupado."
     end
   end
 end
@@ -46,28 +46,33 @@ end
 hotel = Hotel.new
 
 loop do
-  puts 'O que deseja? Reservar, Cancelar, Buscar, Sair  '
-  comando = gets.chomp.downcase
+  # puts 'O que deseja? Reservar, Cancelar, Buscar, Sair  '
+  puts 'Escolha uma opção:'
+  puts '1. Reservar'
+  puts '2. Cancelar'
+  puts '3. Buscar'
+  puts '4. Sair'
+  command = gets.chomp.to_i
 
-  case comando
-  when 'reservar'
+  case command
+  when 1
     puts 'Digite o número do quarto a ser reservado:'
-    quarto = gets.to_i
-    hotel.reservar(quarto)
-  when 'cancelar'
+    room = gets.to_i
+    hotel.reserve(room)
+  when 2
     puts 'Digite o número do quarto a ser cancelado:'
-    quarto = gets.to_i
-    hotel.cancelar(quarto)
-  when 'buscar'
+    room = gets.to_i
+    hotel.cancel(room)
+  when 3
     puts 'Digite o número do quarto a ser buscado:'
-    quarto = gets.to_i
-    hotel.buscar(quarto)
-  when 'sair'
+    room = gets.to_i
+    hotel.search(room)
+  when 4
     break
   end
 
   puts 'Quartos disponíveis:'
-  puts hotel.quartos_disponiveis
+  puts hotel.available_rooms
   puts 'Quartos ocupados:'
-  puts hotel.quartos_ocupados
+  puts hotel.occupied_rooms
 end
